86db1dd64ef01cca4da4d53b41120c488ec383b6
Armando <jarodriguezvera@gmail.com>
2022-05-02 12:22:03 -0500

    Agregar archivos de proyecto.

---
+++ Ordenamiento.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OrdenamientoYBusquedaG2_2022_II
+{
+    internal static class Ordenamiento
+    {
+        public static void Burbuja(int [] A )
+        {
+            for (int j = 0; j < A.Length; j++)//Repite las Pasadas
+            {
+                for (int i = 0; i < A.Length - 1; i++) // Primera Pasada
+                {
+                    int Aux;
+                    if (A[i] > A[i + 1]) //Hay intercambio de elementos
+
+                    {
+                        Aux = A[i];
+                        A[i] = A[i + 1];
+                        A[i + 1] = Aux;
+                    }
+                }
+            }
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace OrdenamientoYBusquedaG2_2022_II
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingresa el tamaño del vector");
+            int N =int.Parse( Console.ReadLine());
+            int[] A = GeneraAleatoria(N);
+            Console.WriteLine("************Desordenada****************");
+            Imprime(A);
+            Console.WriteLine("************Ordenada*******************");
+            Ordenamiento.Burbuja(A);
+            Imprime(A);
+
+            Console.ReadLine();
+
+
+        }
+
+        static int [] GeneraAleatoria( int N)
+        {
+            Random aleatoria = new Random();
+            int [] Aux= new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Aux[i] =(int)(aleatoria.NextDouble()*10000); //Genera un número entre 0 y 10000
+            }
+            return Aux;
+        }
+
+        static void Imprime(int [] A)
+        {
+            foreach( int a in A)
+            {
+                Console.Write("{0} ", a);
+            }
+            Console.WriteLine();
+        }
+    }
+}
